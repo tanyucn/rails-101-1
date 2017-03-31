d657da9dc83c1319c434c8ef5f053e2e46d10709
谭宇 <tanyucn@outlook.com>
2017-03-31 09:27:33 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :group_relationships
-         has_many :participated_groups, :through => :group_relationships, :source => :group
-            def is_member_of?(group)
-               participated_groups.include?(group)
-            end
-def join!(group)
-  participated_groups << group
-end
+  has_many :groups
+  has_many :posts
 
-def quit!(group)
-  participated_groups.delete(group)
-end
+
+has_many :group_relationships
+has_many :participated_groups, :through => :group_relationships, :source => :group
+
+    def is_member_of?(group)
+      participated_groups.include?(group)
+    end
+    def join!(group)
+   participated_groups << group
+ end
+
+ def quit!(group)
+   participated_groups.delete(group)
+ end
 end
